281333570254f08d81300a5190a1f94f801763e1
swang-va <swang@vendasta.com>
2020-09-12 11:29:35 -0600

    Add pypi project description

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), 'r') as f:
+    long_description = f.read()
 
 setup(
     name='ai1wm',
@@ -6,10 +11,12 @@ setup(
     version='1.2',
     license='MIT',
     description='Packs/Unpacks `All-in-One WP Migration` packages',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Sander Wang',
     author_email='sw.online@outlook.com',
     url='https://github.com/sanderwang/ai1wm',
-    download_url='https://github.com/sanderwang/ai1wm/archive/1.1.tar.gz',
+    download_url='https://github.com/sanderwang/ai1wm/archive/1.2.tar.gz',
     keywords=['WordPress', 'All-in-One WP Migration'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
